302db8349efba622ed2c00e5c8f1bea36fe813f0
masumkawser <kawser151722@gmail.com>
2022-08-23 11:17:47 +0600

    listingCardAdded

--- budget.js
+++ budget.js
@@ -1,20 +1,58 @@
-document.getElementById('Calculate').addEventListener('click',function(){
-
-const perplayer=document.getElementById('player');
-const perplayerString=perplayer.value ;
-const perplayerAmount=parseFloat(perplayerString);
-
-perplayer.value ='' ;
-console.log(perplayerAmount);
 
 
-const playerDam=document.getElementById('pexpense');
-const playerDamString=playerDam.innerText;
-const playerDamTaka=parseFloat(playerDamString);
-const playerExpense=perplayerAmount*5;
-playerDam.innerText=playerExpense;
+function getInputValue(inputId){
+    const inputFeild = document.getElementById(inputId);
+    const inputString = inputFeild.value;
+    const currentInput = parseFloat(inputString);
+    // console.log(currentInput);
+
+    return currentInput;
+}
+function getTextValue(inputId){
+    const textElement = document.getElementById(inputId);
+    const textElementString = textElement.innerText;
+    const currentElement = parseFloat(textElementString);
+    // console.log(currentInput);
+
+    return  currentElement ;
+}
+
+
+function setElementValue(elementId, value){
+
+const TestValue = document.getElementById(elementId);
+TestValue.innerText =value;
+
+
+}
+
+
+
+document.getElementById('Calculate').addEventListener('click',function (){
+    
+
+
+const newPlayer = getInputValue('player');
+
+const perPlayerPrice = newPlayer * 5;
+
+setElementValue('pexpense', perPlayerPrice);
+
 
 })
 
+document.getElementById('Total-Calculate').addEventListener('click', function(){
+
+const TotalplayerPrice = getTextValue('pexpense');
+
+const manager = getInputValue('manager');
+
+const coach = getInputValue('Coach');
+
+const totalPrice = manager + coach + TotalplayerPrice  ;
+
+setElementValue('Total', totalPrice);
 
 
+
+})
---
+++ listing.js
@@ -0,0 +1,66 @@
+
+// addTolist(this)
+// {
+
+//     this.disabled =true;
+    
+// }
+const cart = [];
+
+function displayProduct()
+{
+    let totalPrice = 0;
+    const totalProducts = document.getElementById("total-player");
+    totalProducts.innerText = cart.length;
+
+    const cartContainer = document.getElementById("player-cart");
+    cartContainer.textContent = '';
+
+    for(let i = 0; i < cart.length; i++)
+    {
+if(cart.length>5){
+
+    alert('you can not select more than 5')
+
+    return ;
+}
+
+        totalPrice += cart[i].pdPrice;
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i+1}</th>
+        <td>${cart[i].pdName}</td>
+        
+        `;
+        cartContainer.appendChild(tr);
+    }
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    
+    `;
+
+    cartContainer.appendChild(tr);
+}
+
+function addTolist(element) {
+    const pdName = element.parentNode.parentNode.children[1].innerText;
+    
+    const pd = {
+        pdName: pdName,
+        
+    }
+    cart.push(pd);
+
+    displayProduct();
+}
+
+
+
+
+
+
+
+
+
+
